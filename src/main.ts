459cec210135f23280d42f8652843607f6b45150
Justin-Lam <justinflam9225k@gmail.com>
2024-10-13 18:07:34 -0700

    Added app title to webpage

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+
+const appTitle = document.createElement("h1");
+appTitle.innerHTML = APP_NAME;
+app.append(appTitle);
