d2cd260b063836a8b2b833b38c50c0fb228bb65a
Justin-Lam <justinflam9225k@gmail.com>
2024-10-13 18:51:48 -0700

    Allowed the user to draw on the canvas using their mouse

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 //app.innerHTML = APP_NAME;
 
-
 // Variables
 const CANVAS_WIDTH = 256;
 const CANVAS_HEIGHT = CANVAS_WIDTH;
@@ -16,8 +15,31 @@ const appTitle: HTMLHeadingElement = document.createElement("h1");
 appTitle.innerHTML = APP_NAME;
 app.append(appTitle);
 
-// Canvas
+// Canvas, Context, Cursor
 const canvas: HTMLCanvasElement = document.createElement("canvas");
+const ctx = canvas.getContext("2d");		// ctx = "context" aka "CanvasRenderingContext2D object"
+if (!ctx) { throw new Error("Failed to get 2D context"); }		// ensure that we got something back from getContext(); brace told me to add this to remove warnings
+const cursor = { active: false, x: 0, y: 0 };
 canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT
+canvas.height = CANVAS_HEIGHT;
+canvas.addEventListener("mousedown", (e) => {
+	// e = "event object"
+	cursor.active = true;
+	cursor.x = e.offsetX;
+	cursor.y = e.offsetY;
+});
+canvas.addEventListener("mousemove", (e) => {
+	// e = "event object"
+	if (cursor.active) {
+		ctx.beginPath();
+		ctx.moveTo(cursor.x, cursor.y);
+		ctx.lineTo(e.offsetX, e.offsetY);
+		ctx.stroke();
+		cursor.x = e.offsetX;
+		cursor.y = e.offsetY;
+	}
+});
+canvas.addEventListener("mouseup", (e) => {
+	cursor.active = false;
+});
 app.append(canvas);
