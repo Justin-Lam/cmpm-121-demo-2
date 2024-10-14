f83ede561c050822350e6998542cb59784de3161
Justin-Lam <justinflam9225k@gmail.com>
2024-10-13 18:18:34 -0700

    Added a canvas

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import "./style.css";
 
-const APP_NAME = "Sticker Sketchpad";
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const APP_NAME: string = "Sticker Sketchpad";
+const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 //app.innerHTML = APP_NAME;
 
 
-const appTitle = document.createElement("h1");
+// Variables
+const CANVAS_WIDTH = 256;
+const CANVAS_HEIGHT = CANVAS_WIDTH;
+
+// App Title
+const appTitle: HTMLHeadingElement = document.createElement("h1");
 appTitle.innerHTML = APP_NAME;
 app.append(appTitle);
+
+// Canvas
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT
+app.append(canvas);
