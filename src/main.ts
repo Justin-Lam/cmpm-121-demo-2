79a416491f415c01275ee40ca178ed79fabe2456
Justin-Lam <justinflam9225k@gmail.com>
2024-10-13 21:50:48 -0700

    Updated clear button and refactored code; Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,13 @@ document.title = APP_NAME;
 //app.innerHTML = APP_NAME;
 
 // Interfaces
+interface Point {
+	x: number,
+	y: number
+}
 interface Cursor {
 	active: boolean;
-	x: number;
-	y: number;
+	pos: Point	// "position"
 }
 
 // Variables
@@ -27,10 +30,10 @@ app.append(appTitle);
 // create variables
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d"); // ctx = "context" aka "CanvasRenderingContext2D object"
-const cursor: Cursor = { active: false, x: 0, y: 0 };
-const lines = []; // array of lines, where a line is an array of points
-let currentLine = []; //  represents the user's current line when they're drawing; contains the points from mouse down to mouse up
-const drawingChangedEvent = new Event("drawing-changed");
+const cursor: Cursor = { active: false, pos: {x: 0, y: 0} };
+let lines: Point[][] = []; // array of lines, where a line is an array of points
+let currentLine: Point[] = []; //  represents the user's current line when they're drawing; contains the points from mouse down to mouse up
+const drawingChangedEvent: Event = new Event("drawing-changed");
 
 // set components of variables
 canvas.width = CANVAS_WIDTH;
@@ -41,20 +44,20 @@ if (ctx == null) throw new Error("ctx is null"); // ensure that we got something
 canvas.addEventListener("mousedown", (e) => {
 	// set cursor
 	cursor.active = true;
-	cursor.x = e.offsetX;
-	cursor.y = e.offsetY;
+	cursor.pos.x = e.offsetX;
+	cursor.pos.y = e.offsetY;
 	// add a line to lines
 	lines.push(currentLine);
 	// enter the first point into currentLine
-	currentLine.push({ x: cursor.x, y: cursor.y });
+	currentLine.push({ x: cursor.pos.x, y: cursor.pos.y });
 	canvas.dispatchEvent(drawingChangedEvent);
 });
 canvas.addEventListener("mousemove", (e) => {
 	if (cursor.active) {
 		// push the point of where the mouse currently is now to currentLine and then redraw
-		cursor.x = e.offsetX;
-		cursor.y = e.offsetY;
-		currentLine.push({ x: cursor.x, y: cursor.y });
+		cursor.pos.x = e.offsetX;
+		cursor.pos.y = e.offsetY;
+		currentLine.push({ x: cursor.pos.x, y: cursor.pos.y });
 		canvas.dispatchEvent(drawingChangedEvent);
 	}
 });
@@ -66,21 +69,21 @@ canvas.addEventListener("drawing-changed", (e) => {
 	// this is the redraw() function from paint1.html
 
 	// clear the canvas so we can redraw the lines
-	ctx?.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	// redraw the lines
 	for (const line of lines) {
 		// if the user just clicks on the canvas, it creates a line with just one point in it
 		// we don't want to draw those since a line needs two points (otherwise it won't know its direciton)
 		if (line.length > 1) {
-			ctx?.beginPath();
+			ctx.beginPath();
 			// move to the first point
-			const { x, y } = line[0];
-			ctx?.moveTo(x, y);
+			const point: Point = line[0];
+			ctx.moveTo(point.x, point.y);
 			// connect the points to make the line
-			for (const { x, y } of line) {
-				ctx?.lineTo(x, y);
+			for (const point of line) {
+				ctx.lineTo(point.x, point.y);
 			}
-			ctx?.stroke();
+			ctx.stroke();
 		}
 	}
 });
@@ -91,6 +94,13 @@ const clearButton: HTMLButtonElement = document.createElement("button");
 clearButton.innerHTML = "Clear";
 clearButton.addEventListener("click", () => {
 	// this was copied from https://glitch.com/~quant-paint
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	lines = [];
+	canvas.dispatchEvent(drawingChangedEvent);
 });
 app.append(clearButton);
+
+// Functions
+function redraw(): void
+{
+	
+}
