0698365a4125c670e8666212a548aafbd3d424ed
Justin-Lam <justinflam9225k@gmail.com>
2024-10-18 11:53:05 -0700

    Implemented placing stickers; Step 8 complete

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,11 @@ const APP_NAME: string = "Sticker Sketchpad";
 const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 
-// Variables
+// Constants
 const CANVAS_WIDTH: number = 256;
-const CANVAS_HEIGHT: number = CANVAS_WIDTH;	// square canvas
-const THIN = 1;	// line width
-const THICK = 4;	// line width
-let markerSelected = true;	// thin marker selected by default
-let lineWidth = THIN;	// selected by default
-let sticker = "";
+const CANVAS_HEIGHT: number = CANVAS_WIDTH; // square canvas
+const THIN = 1; // line width
+const THICK = 4; // line width
 
 // Interfaces
 interface Point { // a Line would be an array of points: Point[]
@@ -26,7 +23,10 @@ interface Point { // a Line would be an array of points: Point[]
 // using classes like in paint2.html or using functional programming like in Functions are the Ultimate Commands (TS Playground)
 // I'm choosing to go with functional programming because it seems to me like the "JavaScript/TypeScript" way of completing this task
 type RenderThingCommand = (ctx: CanvasRenderingContext2D) => void;
-function makeRenderLineCommand(line: Point[], width: number): RenderThingCommand {
+function makeRenderLineCommand(
+	line: Point[],
+	width: number,
+): RenderThingCommand {
 	return (ctx: CanvasRenderingContext2D) => {
 		// we can be sure line is a real line that contains 2+ points because the canvas's mouseup event handles that
 		// set line width
@@ -42,7 +42,7 @@ function makeRenderLineCommand(line: Point[], width: number): RenderThingCommand
 		}
 		// show the line
 		ctx.stroke();
-	}
+	};
 }
 function makeRenderToolPreviewCommand(pos: Point, radius: number) {
 	return (ctx: CanvasRenderingContext2D) => {
@@ -51,16 +51,16 @@ function makeRenderToolPreviewCommand(pos: Point, radius: number) {
 		// start a new line
 		ctx.beginPath();
 		// draw the circle
-		ctx.ellipse(pos.x, pos.y, radius, radius, 0, 0, 2*Math.PI);
+		ctx.ellipse(pos.x, pos.y, radius, radius, 0, 0, 2 * Math.PI);
 		// show the circle
 		ctx.stroke();
-	}
+	};
 }
-function makeRenderStickerCommand(pos: Point, sticker: string) {	// used for sticker preview and actual placed stickers
+function makeRenderStickerCommand(pos: Point, sticker: string) { // used for sticker preview and placing stickers
 	return (ctx: CanvasRenderingContext2D) => {
 		ctx.font = "32px monospace";
-        ctx.fillText(sticker, pos.x - 21, pos.y + 12);
-	}
+		ctx.fillText(sticker, pos.x - 21, pos.y + 12); // center the sticker preview at the tip of the cursor
+	};
 }
 
 // App Title
@@ -76,6 +76,9 @@ if (ctx == null) throw new Error("ctx is null"); // ensure that we got something
 let displayCommands: RenderThingCommand[] = []; // lines that should be displayed
 let redoCommands: RenderThingCommand[] = []; // lines that have been undone
 let currentLine: Point[] = []; //  represents the user's current line when they're drawing; contains the points from mouse down to mouse up
+let currentLineWidth = THIN; // thin by default
+let currentSticker = "";
+let markerSelected = true; // selected by default
 let showToolPreviewCommand: RenderThingCommand | null = null;
 let showStickerPreviewCommand: RenderThingCommand | null = null;
 const drawingChangedEvent: Event = new Event("drawing-changed");
@@ -87,55 +90,102 @@ canvas.height = CANVAS_HEIGHT;
 
 // create drawing canvas events
 canvas.addEventListener("mousedown", (e: MouseEvent) => {
-	// enter the first point into currentLine
-	currentLine.push({ x: e.offsetX, y: e.offsetY });
-	// convert currentLine into a line command, and enter that command into displayCommands so it can be popped in the mouse move or mouse up events
-	displayCommands.push(makeRenderLineCommand(currentLine, lineWidth));
-	// hide the tool and/or sticker preview
-	showToolPreviewCommand = null;
-	showStickerPreviewCommand = null;
+	// drawing
+	if (markerSelected) {
+		// enter the first point into currentLine
+		currentLine.push({ x: e.offsetX, y: e.offsetY });
+		// convert currentLine into a line command, and enter that command into displayCommands so it can be popped in the mouse move or mouse up events
+		displayCommands.push(
+			makeRenderLineCommand(currentLine, currentLineWidth),
+		);
+		// hide the tool preview
+		showToolPreviewCommand = null;
+		canvas.dispatchEvent(drawingChangedEvent);
+	} // placing stickers
+	else {
+		// place sticker
+		displayCommands.push(
+			makeRenderStickerCommand(
+				{ x: e.offsetX, y: e.offsetY },
+				currentSticker,
+			),
+		);
+		// hide the sticker preview
+		showStickerPreviewCommand = null;
+	}
+
+	// dispatch drawing changed event
 	canvas.dispatchEvent(drawingChangedEvent);
 	// clear redoCommands
 	redoCommands = [];
 });
 canvas.addEventListener("mousemove", (e: MouseEvent) => {
 	// draw
-	if (e.buttons == 1) {	// left mouse button down
+	if (markerSelected && e.buttons == 1) { // left mouse button down
 		// push the cursor's position into currentLine
 		currentLine.push({ x: e.offsetX, y: e.offsetY });
 		// replace the current line command with a new one with an updated currentLine
 		displayCommands.pop();
-		displayCommands.push(makeRenderLineCommand(currentLine, lineWidth));
+		displayCommands.push(
+			makeRenderLineCommand(currentLine, currentLineWidth),
+		);
 		// dispatch drawing changed event
 		canvas.dispatchEvent(drawingChangedEvent);
-	}
-	// make and show tool preview
+	} // make and show tool preview
 	else if (markerSelected) {
-		showToolPreviewCommand = makeRenderToolPreviewCommand({ x: e.offsetX, y: e.offsetY }, lineWidth);
+		showToolPreviewCommand = makeRenderToolPreviewCommand({
+			x: e.offsetX,
+			y: e.offsetY,
+		}, currentLineWidth);
 		canvas.dispatchEvent(toolMovedEvent);
-	}
-	// make and show sticker preview
+	} // move placed sticker
+	else if (e.buttons == 1) { // left mouse button down
+		// replace the current sticker command with a new one with an updated position
+		displayCommands.pop();
+		displayCommands.push(
+			makeRenderStickerCommand(
+				{ x: e.offsetX, y: e.offsetY },
+				currentSticker,
+			),
+		);
+		canvas.dispatchEvent(toolMovedEvent);
+	} // make and show sticker preview
 	else {
-		showStickerPreviewCommand = makeRenderStickerCommand( { x: e.offsetX, y: e.offsetY }, sticker);
+		showStickerPreviewCommand = makeRenderStickerCommand({
+			x: e.offsetX,
+			y: e.offsetY,
+		}, currentSticker);
 		canvas.dispatchEvent(toolMovedEvent);
 	}
 });
-canvas.addEventListener("mouseup", () => {
+canvas.addEventListener("mouseup", (e: MouseEvent) => {
 	// get rid of the most recent line command if the line is just a point and therefore isn't proper
 	if (currentLine.length == 1) {
 		displayCommands.pop();
 	}
-	// show tool preview
-	canvas.dispatchEvent(toolMovedEvent);
 	// reset currentLine
 	currentLine = [];
+	// make and show tool/sticker preview
+	if (markerSelected) {
+		showToolPreviewCommand = makeRenderToolPreviewCommand({
+			x: e.offsetX,
+			y: e.offsetY,
+		}, currentLineWidth);
+		canvas.dispatchEvent(toolMovedEvent);
+	} else {
+		showStickerPreviewCommand = makeRenderStickerCommand({
+			x: e.offsetX,
+			y: e.offsetY,
+		}, currentSticker);
+		canvas.dispatchEvent(toolMovedEvent);
+	}
 });
 canvas.addEventListener("mouseout", () => {
 	// hide the tool and sticker preview
 	showToolPreviewCommand = null;
 	showStickerPreviewCommand = null;
 	canvas.dispatchEvent(drawingChangedEvent);
-  });
+});
 canvas.addEventListener("drawing-changed", () => {
 	redraw();
 });
@@ -153,17 +203,17 @@ function redraw() {
 	}
 	// clear the canvas so we can redraw the lines and/or cursor
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// draw the lines
-	for (const lineCommand of displayCommands) {
-		lineCommand(ctx); // execute
+	// execute the display commands
+	for (const command of displayCommands) {
+		command(ctx); // execute
 	}
 	// draw the cursor
-	if (showToolPreviewCommand) {	// != null
-		showToolPreviewCommand(ctx);	// execute
+	if (showToolPreviewCommand) { // != null
+		showToolPreviewCommand(ctx); // execute
 	}
 	// draw the sticker
-	if (showStickerPreviewCommand) {	// != null
-		showStickerPreviewCommand(ctx);	// execute
+	if (showStickerPreviewCommand) { // != null
+		showStickerPreviewCommand(ctx); // execute
 	}
 }
 app.append(canvas);
@@ -183,11 +233,11 @@ app.append(clearButton);
 const undoButton: HTMLButtonElement = document.createElement("button");
 undoButton.innerHTML = "Undo";
 undoButton.addEventListener("click", () => {
-	// attempt to get the lastest line command while removing it from displayCommands
-	const lastLineCommand: RenderThingCommand | undefined = displayCommands.pop();
-	if (lastLineCommand != undefined) {
+	// attempt to get the lastest command while removing it from displayCommands
+	const lastCommand: RenderThingCommand | undefined = displayCommands.pop();
+	if (lastCommand != undefined) {
 		// add the command to redoCommands
-		redoCommands.push(lastLineCommand);
+		redoCommands.push(lastCommand);
 		// dispatch drawing changed event
 		canvas.dispatchEvent(drawingChangedEvent);
 	}
@@ -198,11 +248,11 @@ app.append(undoButton);
 const redoButton: HTMLButtonElement = document.createElement("button");
 redoButton.innerHTML = "Redo";
 redoButton.addEventListener("click", () => {
-		// attempt to get the lastest line command while removing it from redoCommands
-	const lastRedoLineCommand: RenderThingCommand | undefined = redoCommands.pop();
-	if (lastRedoLineCommand != undefined) {
+	// attempt to get the lastest command while removing it from redoCommands
+	const lastCommand: RenderThingCommand | undefined = redoCommands.pop();
+	if (lastCommand != undefined) {
 		// add the command to displayCommands
-		displayCommands.push(lastRedoLineCommand);
+		displayCommands.push(lastCommand);
 		// dispatch drawing changed event
 		canvas.dispatchEvent(drawingChangedEvent);
 	}
@@ -212,9 +262,9 @@ app.append(redoButton);
 // Thin Button
 const thinButton: HTMLButtonElement = document.createElement("button");
 thinButton.innerHTML = "Thin";
-thinButton.disabled = true;	// thin is selected initially thus this button is disabled initially
+thinButton.disabled = true; // thin is selected initially thus this button is disabled initially
 thinButton.addEventListener("click", () => {
-	lineWidth = THIN;
+	currentLineWidth = THIN;
 	markerSelected = true;
 	thinButton.disabled = true;
 	thickButton.disabled = false;
@@ -229,7 +279,7 @@ app.append(thinButton);
 const thickButton: HTMLButtonElement = document.createElement("button");
 thickButton.innerHTML = "Thick";
 thickButton.addEventListener("click", () => {
-	lineWidth = THICK;
+	currentLineWidth = THICK;
 	markerSelected = true;
 	thinButton.disabled = false;
 	thickButton.disabled = true;
@@ -244,7 +294,7 @@ app.append(thickButton);
 const faceButton: HTMLButtonElement = document.createElement("button");
 faceButton.innerHTML = "😊";
 faceButton.addEventListener("click", () => {
-	sticker = "😊";
+	currentSticker = "😊";
 	markerSelected = false;
 	thinButton.disabled = false;
 	thickButton.disabled = false;
@@ -258,7 +308,7 @@ app.append(faceButton);
 const heartButton: HTMLButtonElement = document.createElement("button");
 heartButton.innerHTML = "💖";
 heartButton.addEventListener("click", () => {
-	sticker = "💖";
+	currentSticker = "💖";
 	markerSelected = false;
 	thinButton.disabled = false;
 	thickButton.disabled = false;
@@ -272,7 +322,7 @@ app.append(heartButton);
 const starButton: HTMLButtonElement = document.createElement("button");
 starButton.innerHTML = "⭐";
 starButton.addEventListener("click", () => {
-	sticker = "⭐";
+	currentSticker = "⭐";
 	markerSelected = false;
 	thinButton.disabled = false;
 	thickButton.disabled = false;
